5e5a16ff0a914d991a19f94551cd74155fb856cc
Abhishek Shakya <abshakya63@GMAIL.COM>
2025-11-20 20:30:50 +0530

    Add files via upload

---
+++ One-dimensional Array/first.m
@@ -0,0 +1,17 @@
+% a = [];  
+% disp(isempty(a));
+
+arr1 = [1,2,9,4]; % row vector
+% disp(arr1)
+
+arr2 = [5;
+        6;
+        7;
+        8]; % column vector
+% disp(arr2);
+
+% Access the element with help of index
+% disp(arr1(3));
+disp(arr1);
+arr1(3) = 3;
+disp(arr1);
