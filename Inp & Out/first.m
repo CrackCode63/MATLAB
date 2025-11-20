1b33a70eb91c171113466e3cd40f36d018f8ef28
Abhishek Shakya <abshakya63@GMAIL.COM>
2025-11-20 20:30:31 +0530

    Add files via upload

---
+++ Inp & Out/first.m
@@ -0,0 +1,14 @@
+% x = 2;
+% y = 4;
+% disp(x + y);
+
+% a = input("Enter the number:");
+% b = input("Enter the number:");
+
+% disp(a + b);
+
+% disp(name);
+
+name = input("What is your name:");
+result = input("Enter your percentage:");
+fprintf("Your name is %s and your percent is %.2f.",name,result);
