da0e952b372dd7d7d59314df2a2ca2e04c516a16
Abhishek Shakya <abshakya63@GMAIL.COM>
2025-11-20 20:30:12 +0530

    Add files via upload

---
+++ Operators/ops.m
@@ -0,0 +1,43 @@
+% 1 . Arth
+
+% a = 2;
+% b = 3;
+% disp(a + b)
+% disp(a - b)
+% disp(a * b)
+% disp(a / b)
+
+% 2. Relational 
+% a = 2;
+% b = 3;
+
+% disp(a < b)
+% disp(a <= b)
+% disp(a > b)
+% disp(a >= b)
+% disp(a == b)
+
+% 3. Logical
+
+% a = 1;
+% b = 0;
+
+
+% disp(a & b) % AND 
+% disp(a | b) % OR
+% disp(~(a | b)) % NOT 
+% disp(xor(a,b))
+
+% 4. MATRIX ops
+
+% mat1 = [1,2,3;4,5,6]; % 2 * 3
+
+% mat2 = [3,5,2;7,9,8]; % 2 * 3
+
+% % % disp(mat1 .* mat2)
+% % disp(mat1 .+ mat2)
+% % % disp(mat1 ./ mat2)
+% disp(mat1 + mat2)
+% disp(mat1 / mat2)
+
+
