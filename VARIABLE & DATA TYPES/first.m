1a991f76716e916cd003746395428cd9fce2ed2c
Abhishek Shakya <abshakya63@GMAIL.COM>
2025-11-20 20:29:22 +0530

    Add files via upload

---
+++ VARIABLE & DATA TYPES/first.m
@@ -0,0 +1,50 @@
+
+x = 2;
+disp(x)
+% disp(x+3);
+% disp(x);
+
+% age = 23;
+% disp(age);
+
+% % types
+
+% % local 
+ 
+% function get()
+%     class = "MCA";
+%     disp(class)
+% end
+
+% get()
+
+% % disp(class)
+
+% % global var
+
+% global y
+% y = 6;
+% function put()
+%     global y
+%     disp(y) 
+% end
+
+% put()
+
+% disp(y); 
+
+% % persistent var
+% function count()
+%     persistent z;
+%     if isempty(z)
+%         z = 0;
+%     end
+%     z = z + 1;
+%     disp(z)
+% end
+
+% count()
+% count()
+% count()
+
+
